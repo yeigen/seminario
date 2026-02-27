3c0b648dcdbe7fd8b66a3dbba433897d36140854
yeigen <gabriel.dev.edwards@gmail.com>
2026-02-26 22:47:18 -0500

    feat: agregar configuración global centralizada y mapa de fuentes de datos
    - config/global.py: constantes, paths, variables de entorno, parámetros
      del pipeline (OAuth, Drive API, rutas, años, MIME types, etc.)
    - config/sources.py: IDs de archivos y carpetas de Google Drive por
      dataset (SNIES, matrícula total, ICFES, PND)

---
+++ config/sources.py
@@ -0,0 +1,92 @@
+FOLDER_ID = "1tcRwkHfaMpu2TUoNgU_GU8SwUGz9WBGK"
+
+SNIES_FOLDER_ID = "1ti0l0DsQm3ct8IE37OhMEwRJI2vY-WRE"
+MATRICULA_TOTAL_FOLDER_ID = "1fTj8K_or6h5rYICnFbcdeB2x0DjWksc-"
+ICFES_FOLDER_ID = "17ZdyGTKGF-7lqYt5U41Wsb4j0C7XUsjW"
+PND_FOLDER_ID = "1dDAr1pTdVX3swGKC5G5rNSqTKW1JT8G7"
+
+PRIORITY_FILES = {
+    "snies": {
+        "administrativos": {
+            "2018": "1eqry9JUwp9-gRjHmHRjWSYQQqh-UhruE",
+            "2019": "1iEtx8ZXPTPxV-Fy0siqdH0SxhspFrICS",
+            "2020": "14pousi1TN7_UTr6sGdN6rFb7pLRkLu5m",
+            "2021": "1e5uRzqe1wYUFQyGN1n8uhhCDxVxfD3Vs",
+            "2022": "1aLK6zPU6A2kgRQ28X8d1Clhma_Y1E6pC",
+            "2023": "1bScCRFdJYxhYYdaiwZLbPOv_WG2oLA3s",
+            "2024": "19WtjTaWUfpBlBsxiWPc_Ze91rU7caPFn",
+        },
+        "admitidos": {
+            "2018": "1QWnaGMfJrxj1BgQiWLflukNamBRiNqig",
+            "2019": "168imP28_eKYIc_wun2OelQXHFyhq0Z1R",
+            "2020": "1gBXRamBb60gcOyz3DktRQ8XjslTaYa_H",
+            "2021": "1QjmVEMgRZAnUkcZdyyto2dr6sr8201-2",
+            "2022": "18q08HzAVuuhs7CBQ99Mpk6jRZZtydX87",
+            "2023": "1Bah8ycayUGP8NRCC0alOKq-YfjgRaAWi",
+            "2024": "1gbL7Z-cK1fw4psP7J9coYsbWxCVNRvyF",
+        },
+        "docentes": {
+            "2018": "1g3n4u5Ca8Sy3Z7aGwd0YkpMlF08wxSVm",
+            "2019": "1lgprXFyom5i2h5W7lkYBvdYy8TxaA1cV",
+            "2020": "1nCMjGpkx7o0YlUT9GDlZeKE_lXVK-1CZ",
+            "2022": "1FbKgBvLaxcRmRhHcJTZjJzDS9_gLAk2M",
+            "2023": "10FVFpAAs9Jsewf5-2wzecDN0TV6vafbO",
+            "2024": "1uhEP7zMyw1MG7Muu3IQ-oGgSom7fwd5v",
+        },
+        "graduados": {
+            "2018": "1WXfJXBAuMMObrSaXUWy1pKvQqcerfDEg",
+            "2019": "1vJNqW7ySusx2BForsvYirFo4Ev-DSa1F",
+            "2020": "1AeDdwX9mVDiyG2aCE14XdhENVh7QqLBy",
+            "2021": "1p72r7b6BY8TFUMKScmw6WaeWgmSQ-Xb9",
+            "2022": "1Eyva7rkqUhIpnU-0StxCT69NZqy6Iha9",
+            "2023": "1X5dO8iVQ4-XXH9vKqQP4d6gFpBhyS4gS",
+            "2024": "1XhI04E1gu1zQ4WuxIr7y2jVn2bSXFim4",
+        },
+        "inscritos": {
+            "2018": "1b2RE4p2OmfkRZNIn9Xoeg7RvbEAQOoWJ",
+            "2019": "1ogUuJTiohflsBstib4V-cO1UPOgoo5Yq",
+            "2020": "1ySvH6UErw0Hbdjg22bCcIHyYjjVQ2e_n",
+            "2021": "14hUMVICAZF-eg7GDNljikxifn6Tkxri9",
+            "2022": "1FRzfWNOVMrc1PA_qZwbsg8yYsS92Euk5",
+            "2023": "1n2t-pDh0nVKZP0OqQJxQ0p-k3Q-otQ0K",
+            "2024": "1uTfIraRXXofY3V_lgxiw8MgyEqTVGoK6",
+        },
+        "matriculados": {
+            "2018": "1GlbW7ru5pPJW0TiSjQoTlP8eyaX6_-3L",
+            "2019": "1scKY6IA32fl3wSoD9fW9pUDRMSlm2eea",
+            "2020": "1PK8J_IgCoZFsQywE5DqX7eNqvhLDRRmo",
+            "2021": "1_-2q4e4z9GJIKibfTpEvXTMs_DDXb-OL",
+            "2022": "12SmYGx_G0UCblA9BedU8776I4Yq4Xbz2",
+            "2023": "1ist9DABxjnaY-H-hm0mC4rEmMGr0StE3",
+            "2024": "1j9I5_FBb9Qf1CVhyBkVCLNMBZUWCsufA",
+        },
+        "matriculados_primer_curso": {
+            "2018": "1yeWuWCwa4i7N66lNt2MIljdXTpI-5ch_",
+            "2019": "1_CACAUtTJ-1wRkB2y-nRXEP02Tzv92Ej",
+            "2020": "1LSmSSdr0hxjjzfy_Qtwe4zbjT5AyqOwd",
+            "2021": "1W8O8tGOTPCFfgvJSA02maRRxZudsdb-A",
+            "2022": "1cYw4l7zNFZHbn42NTuRMDFB3cTA3LsTS",
+            "2023": "1Cfcnalwt84XCsKHa6fjHL4md7wnfBQZB",
+            "2024": "1-jQvRm1L02EhvCM8Ng_g3REjsqCTu4ER",
+        },
+    },
+    "matricula_total": {
+        "matricula_2022_2025_1": "1CbcIHGlQFuiRumyGdbDwcAR_XIoPkXJ5",
+        "matricula_2022_2025_2": "16dt2EhUbUxSWDdfqDt3pezLhSjDaWkh2",
+    },
+    "icfes": {
+        "saber_359": "1iiZWjH5PqLQ-7Ct1bi9AXyfInQqu7uM2",
+    },
+    "pnd": {
+        "seguimiento_pnd": "1M9XggkKlsvWSt8BWvFSONGaLUTbJrRy8",
+    },
+}
+
+SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
+
+FILE_EXTENSIONS = {
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": ".xlsx",
+    "text/csv": ".csv",
+    "text/plain": ".txt",
+    "application/json": ".json",
+}
